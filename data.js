d6c1da53f9f581524eb1fbd502ee05721e5e6ac4
argo-ws <kendiogra@gmail.com>
2025-03-16 23:25:31 +0700

    upload data bd member aktif per 2025/03/16

---
+++ data.js
@@ -0,0 +1,234 @@
+const birthdays =[
+    {
+      name: "Feni Fitryanti",
+      birthdate: "1999-01-16"
+    },
+    {
+      name: "Shania Gracia Harlan",
+      birthdate: "1999-08-31"
+    },
+    {
+      name: "Gita Sekar Andarini",
+      birthdate: "2001-06-30"
+    },
+    {
+      name: "Angelina Christy",
+      birthdate: "2005-12-05"
+    },
+    {
+      name: "Febriola Sinambela",
+      birthdate: "2005-02-26"
+    },
+    {
+      name: "Freyana Shifa Jayawardana",
+      birthdate: "2006-02-13"
+    },
+    {
+      name: "Helisma Mauludzunia Putri Kurnia",
+      birthdate: "2000-06-15"
+    },
+    {
+      name: "Jessica Chandra",
+      birthdate: "2005-09-23"
+    },
+    {
+      name: "Mutiara Azzahra Umandana",
+      birthdate: "2004-07-12"
+    },
+    {
+      name: "Cornelia Syafa Vanisa",
+      birthdate: "2002-07-26"
+    },
+    {
+      name: "Fiony Alveria Tantri",
+      birthdate: "2002-02-04"
+    },
+    {
+      name: "Lulu Azkiya Salsabila",
+      birthdate: "2002-10-23"
+    },
+    {
+      name: "Indah Cahya Nabila",
+      birthdate: "2001-03-20"
+    },
+    {
+      name: "Kathrina Irene Indarto Putri",
+      birthdate: "2006-07-26"
+    },
+    {
+      name: "Marsha Lenathea Lapian",
+      birthdate: "2006-01-09"
+    },
+    {
+      name: "Amanda Puspita Sukma Mulyadewi",
+      birthdate: "2004-12-17"
+    },
+    {
+      name: "Aurellia",
+      birthdate: "2002-10-29"
+    },
+    {
+      name: "Gabriela Abigail Mewengkang",
+      birthdate: "2006-08-07"
+    },
+    {
+      name: "Indira Putri Seruni",
+      birthdate: "2004-04-26"
+    },
+    {
+      name: "Jesslyn Elly",
+      birthdate: "2001-09-13"
+    },
+    {
+      name: "Raisha Syifa Wardhana",
+      birthdate: "2007-11-11"
+    },
+    {
+      name: "Alya Amanda",
+      birthdate: "2006-08-26"
+    },
+    {
+      name: "Anindya Ramadhani",
+      birthdate: "2005-10-18"
+    },
+    {
+      name: "Cathleen Nixie",
+      birthdate: "2009-05-28"
+    },
+    {
+      name: "Celline Thefani",
+      birthdate: "2007-04-09"
+    },
+    {
+      name: "Chelsea Davina Norman",
+      birthdate: "2009-12-23"
+    },
+    {
+      name: "Chynthia Yaputera",
+      birthdate: "2003-11-22"
+    },
+    {
+      name: "Dena Natalia",
+      birthdate: "2005-12-16"
+    },
+    {
+      name: "Desy Natalia",
+      birthdate: "2005-12-16"
+    },
+    {
+      name: "Gendis Mayrannisa",
+      birthdate: "2010-06-23"
+    },
+    {
+      name: "Grace Octaviani",
+      birthdate: "2007-10-18"
+    },
+    {
+      name: "Greesella Sophina Adhalia",
+      birthdate: "2006-01-10"
+    },
+    {
+      name: "Michelle Alexandra",
+      birthdate: "2009-04-22"
+    },
+    {
+      name: "Abigail Rachel",
+      birthdate: "2008-08-06"
+    },
+    {
+      name: "Adeline Wijaya",
+      birthdate: "2007-09-01"
+    },
+    {
+      name: "Aurhel Alana",
+      birthdate: "2006-09-14"
+    },
+    {
+      name: "Catherina Vallencia",
+      birthdate: "2007-08-21"
+    },
+    {
+      name: "Fritzy Rosmerian",
+      birthdate: "2008-07-28"
+    },
+    {
+      name: "Hillary Abigail",
+      birthdate: "2007-10-19"
+    },
+    {
+      name: "Jazzlyn Trisha",
+      birthdate: "2011-02-16"
+    },
+    {
+      name: "Letycia Moreen",
+      birthdate: "2010-06-07"
+    },
+    {
+      name: "Michelle Levia",
+      birthdate: "2009-01-24"
+    },
+    {
+      name: "Nayla Suji",
+      birthdate: "2007-06-18"
+    },
+    {
+      name: "Nina Tutachia",
+      birthdate: "2009-10-16"
+    },
+    {
+      name: "Oline Manuel",
+      birthdate: "2007-11-03"
+    },
+    {
+      name: "Ribka Budiman",
+      birthdate: "2009-01-13"
+    },
+    {
+      name: "Regina Wilian",
+      birthdate: "2009-12-10"
+    },
+    {
+      name: "Shabilqis Naila",
+      birthdate: "2008-09-01"
+    },
+    {
+      name: "Victoria Kimberly",
+      birthdate: "2010-03-08"
+    },
+    {
+      name: "Astrella Virgiananda",
+      birthdate: "2010-08-06"
+    },
+    {
+      name: "Aulia Riza",
+      birthdate: "2007-07-14"
+    },
+    {
+      name: "Bong Aprilli",
+      birthdate: "2010-04-01"
+    },
+    {
+      name: "Hagia Sopia",
+      birthdate: "2008-07-01"
+    },
+    {
+      name: "Humaira Ramadhani",
+      birthdate: "2011-08-13"
+    },
+    {
+      name: "Jacqueline Immanuela",
+      birthdate: "2009-07-09"
+    },
+    {
+      name: "Jemima Evodie",
+      birthdate: "2009-11-09"
+    },
+    {
+      name: "Mikaela Kusjanto",
+      birthdate: "2007-12-15"
+    },
+    {
+      name: "Nur Intan",
+      birthdate: "2006-02-24"
+    }
+  ];
